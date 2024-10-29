756dc98ea2febc264dec7524c92b766d24d9d791
Sofia Besenski <sofia@super.gd>
2024-10-29 15:00:54 -0700

    Set up Selenium and integration tests
    Nothing is being tested yet.

---
+++ zine_it_django/functional_tests/testing_tools.py
@@ -0,0 +1,19 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.conf import settings
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+
+class SeleniumTestCase(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        service = Service(f"{settings.BASE_DIR}/chromedriver")
+        cls.driver = webdriver.Chrome(service=service, options=options)
+        cls.driver.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
--- zine_it_django/zine_it_django/settings.py
+++ zine_it_django/zine_it_django/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    'functional_tests' 
+]
+
+INSTALLED_APPS = DEFAULT_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
