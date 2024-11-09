71ccff40c1f0f88cd7b86bdee56f0a19f26a1c9b
Sofia Besenski <sofia@super.gd>
2024-11-09 15:45:54 -0800

    Change naming to convention
    We can also then rely on some automatic lookups from django this way.

--- uploader/urls.py
+++ uploader/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("pages", views.IndexPageView.as_view(), name="index_pages"),
-    path("pages/new", views.NewPageView.as_view(), name="new_page"),
+    path("pages", views.PageIndexView.as_view(), name="index_pages"),
+    path("pages/new", views.PageCreateView.as_view(), name="new_page"),
 ]
--- uploader/views.py
+++ uploader/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from uploader.models import Page 
 from uploader.forms import PageForm
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 from django.views.generic import ListView
 
-class NewPageView(FormView):
+class PageCreateView(CreateView):
     template_name = "uploader/pages/new.html"
     form_class = PageForm
     success_url = "/uploader/pages" 
 
-class IndexPageView(ListView):
+class PageIndexView(ListView):
     model = Page 
     template_name = "uploader/pages/index.html"  
