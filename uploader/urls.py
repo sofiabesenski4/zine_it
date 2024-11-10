23cf09a2799543aec86f983f6ea1c64709efbe9b
Sofia Besenski <sofia@super.gd>
2024-11-09 16:22:10 -0800

    Add Edit page for Pages

--- uploader/urls.py
+++ uploader/urls.py
@@ -6,4 +6,5 @@ app_name = "uploader"
 urlpatterns = [
     path("pages", views.PageIndexView.as_view(), name="index_pages"),
     path("pages/new", views.PageCreateView.as_view(), name="new_page"),
+    path("pages/<int:id>/edit", views.PageUpdateView.as_view(), name="edit_page"),
 ]
--- uploader/views.py
+++ uploader/views.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
 from uploader.models import Page 
 from uploader.forms import PageForm
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import ListView
 
 class PageCreateView(CreateView):
     form_class = PageForm
     template_name = "uploader/pages/new.html"
     success_url = "/uploader/pages" 
+
+class PageUpdateView(UpdateView):
+    form_class = PageForm 
+    template_name = "uploader/pages/edit.html"
+    success_url = "/uploader/pages" 
+
+    def get_object(self):
+        page = Page.objects.get(id=self.kwargs["id"])
+        return page 
 
 class PageIndexView(ListView):
     model = Page 
