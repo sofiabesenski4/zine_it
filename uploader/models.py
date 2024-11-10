9fddaf607870ba254b3186dd99b75315840fe3b2
Sofia Besenski <sofia@super.gd>
2024-11-09 16:02:49 -0800

    Show zine name in list

--- uploader/models.py
+++ uploader/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Zine(models.Model):
     name = models.CharField(max_length=70)
+    def __str__(self):
+        return self.name
 
 class Page(models.Model):
     index = models.IntegerField(default=0)
