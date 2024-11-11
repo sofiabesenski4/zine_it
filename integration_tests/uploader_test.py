567c1a0cbc4547148533a72e7da9e196eff8d01c
Sofia Besenski <sofia@super.gd>
2024-11-11 14:05:57 -0800

    Cover Create UX path in feature test

--- integration_tests/uploader_test.py
+++ integration_tests/uploader_test.py
@@ -1,8 +1,24 @@
 from integration_tests.testing_tools import SeleniumTestCase
 from selenium.webdriver.common.by import By
+from uploader.models import Zine
+from selenium.webdriver.support.ui import Select
 
 class UploaderTest(SeleniumTestCase):
     def test_image_uploader_form(self):
+        Zine(name="Test Zine").save()
         # Go to the login page
-        self.driver.get(self.live_server_url + "/uploader")
-        self.driver.find_element(By.LINK_TEXT, "Upload")
+        
+
+        self.driver.get(self.live_server_url + "/uploader/pages")
+        self.driver.find_element(By.LINK_TEXT, "Create a new page").click()
+        self.driver.find_element(By.ID, "id_index").send_keys("1")
+
+        upload_button = self.driver.find_element(By.XPATH, "/html/body/form/input[2]")
+        zine_dropdown = self.driver.find_element(By.ID, "id_zine")
+        select = Select(zine_dropdown)
+
+        select.select_by_visible_text("Test Zine")
+        upload_button.click()
+        self.assertTrue("Page Index" in self.driver.page_source)
+        self.assertTrue("Zine: Test" in self.driver.page_source)
+        self.assertTrue("Index: 1" in self.driver.page_source)
