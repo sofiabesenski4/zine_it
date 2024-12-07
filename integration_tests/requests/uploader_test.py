ed7096e452244e43501190c218b0c22d1f40c3cc
Sofia Besenski <sofia@super.gd>
2024-12-07 14:14:10 -0800

    Add POST request spec for pages

--- integration_tests/requests/uploader_test.py
+++ integration_tests/requests/uploader_test.py
@@ -1,13 +1,13 @@
-from django.test import RequestFactory, TestCase
+from django.test import RequestFactory, TestCase, Client
 from uploader.models import Zine, Page
-from uploader.views import PageIndexView
+from uploader.views import PageIndexView, PageCreateView
 
 class UploaderRequestTest(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
 
-    def test_details(self):
+    def test_index(self):
         my_first_zine = Zine(name="My First Zine")
         my_first_zine.save()
 
@@ -20,5 +20,18 @@ class UploaderRequestTest(TestCase):
         response = PageIndexView.as_view()(request)
         self.assertEqual(response.status_code, 200)
         
-        self.assertEqual(Zine.objects.filter(name="My First Zine").exists(), True)
+        response.render()
 
+        self.assertContains(response, "My First Zine")
+        self.assertContains(response, "Index: 1")
+    
+    def test_create_page(self):
+        my_first_zine = Zine(name="My First Zine")
+        my_first_zine.save()
+
+        request = self.factory.post("/uploader/pages", data={"zine": my_first_zine.id, "index": "2"})
+
+        response = PageCreateView.as_view()(request)
+        response.client = Client()
+        
+        self.assertRedirects(response, "/uploader/pages")
