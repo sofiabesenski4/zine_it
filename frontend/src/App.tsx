6b3121bdf0a9561f6fc4b010e25b85dd42b69ca4
Sofia Besenski <sofia@super.gd>
2024-12-20 23:20:40 -0800

    Add a couple styles to the front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import logo from './logo-black-flag.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App flex flex-col items-center justify-items-center">
+    <div className="App flex flex-col items-center justify-items-center mt-16 gap-12">
       <div className="flex-none">
         <img src={logo} className="App-logo flex-none" alt="logo" />
       </div>
+
       <h1 className="text-3xl font-bold underline">
         Zine it! 
       </h1>
