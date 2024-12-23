ee1640a675303cf813cbf6da275c70b69950f267
Sofia Besenski <sofia@super.gd>
2024-12-22 21:57:29 -0800

    Start building out more components
    Nothing looks good yet, still working on the skeleton.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,8 @@
-import logo from './logo-black-flag.svg'; 
+import blackFlag from './logo-black-flag.svg'; 
+import squatterZ from './squatter-z.svg'; 
 import './App.css';  
 import { ReactElement, useState } from 'react'
 import { useQuery, gql } from "@apollo/client"
-
-const Button = () => {
-  return (
-    <button type="submit"></button>
-  )
-}
 
 const GET_ZINES =   gql`
   query MyQuery {
@@ -23,14 +18,6 @@ const GET_ZINES =   gql`
     }
   }
 `
-const GET_DOG_PHOTO = gql`
-  query Dog($breed: String!) {
-    dog(breed: $breed) {
-      id
-      displayImage
-    }
-  }
-`;
 const GET_ZINE = gql`
   query Zine($name: String!) {
     name
@@ -39,21 +26,34 @@ const GET_ZINE = gql`
 `
 
 type ContainerProps = {
+  onClick: (event: React.MouseEvent<HTMLElement>) => void
   children: ReactElement
 }
 
 const Container: React.SFC<ContainerProps> = (props) => {
   return (
-    <div className="container">
-    { props.children }
+    <div className="rounded-md bg-stone-400 w-36 h-8 hover:bg-sky-700" onClick={props.onClick}>
+      { props.children }
     </div>
+  )
+}
+
+type ButtonProps = {
+  text: string
+  children: ReactElement
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+  return (
+      <button className="bg-stone-300 h-24 w-44 hover:bg-sky-700" type="submit">{props.text}</button>
   )
 }
 
 
 const App = () => {
   const { loading, error, data } = useQuery(GET_ZINES)
-  const [currentZine, setCurrentZine] = useState<string>(null)
+  
+  const [currentZine, setCurrentZine] = useState<Zine>(null)
 
 
   if (loading) return <p>Loading..</p>
@@ -61,28 +61,30 @@ const App = () => {
 
 
   return (
-    <div className="App h-screen w-screen overflow-hidden">
-      <div className="flex flex-col bg-[#1da1f2] items-center justify-center gap-12 h-full">
+    <div className="App h-screen w-screen bg-stone-800 overflow-hidden">
+      <div className="m-auto flex flex-col items-center justify-center gap-12 h-full max-w-52">
         <div className="flex-none">
-          <img src={logo} className="App-logo flex-none h-50 w-50" alt="logo" />
+          <img src={squatterZ} className="App-logo flex-none max-h-52 h-52 w-52" alt="logo" />
         </div>
 
         <div>
-          <h1 className="text-3xl font-bold underline">
+          <h1 className="text-3xl font-bold underline bg-[#1da1f2]">
             Zine it! 
           </h1>
         </div>
+        {currentZine ? <Container><div>{currentZine.name}</div></Container> : null}
 
-        { 
-          currentZine ? <div>{currentZine}</div> : 
-            <div className="zine__listing flex">
-              { data.allZines.map((zine)=>
-                  <Container>
-                    {zine.name}
-                  </Container>
-              )}
-            </div>
-        }
+        <div className="zine__listing flex gap-2">
+          { 
+            data.allZines.map((zine)=>
+              <Container  onClick={() => setCurrentZine(zine)}>
+                <div className="w-22 h-18 ">{zine.name}</div>
+              </Container>
+            )
+          }
+        </div>
+
+        <Button text="Test Button"/>
       </div>
     </div>
   );
