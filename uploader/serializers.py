3c2a8c67efdddf9eb1beb49d2905f1d30b956db8
Sofia Besenski <sofia@super.gd>
2024-12-24 23:23:34 -0800

    Add new Page API boilerplate

--- uploader/serializers.py
+++ uploader/serializers.py
@@ -3,7 +3,13 @@ from .models import Zine, Page
 from rest_framework import serializers
 
 
-class ZineSerializer(serializers.HyperlinkedModelSerializer):
+class ZineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Zine 
         fields = ['id', 'name']
+
+class PageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Page 
+        fields = ['id', 'zine', 'index', 'image']
+
--- uploader/urls.py
+++ uploader/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ZineViewSet
+from .views import ZineViewSet, PageViewSet
 from . import views 
 
 router = routers.DefaultRouter()
-router.register(r'zines', ZineViewSet)
+router.register(r'^zines', ZineViewSet)
+router.register(r'^pages', PageViewSet)
 
 app_name = "uploader"
 
