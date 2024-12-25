84fe63350e619e5717c2c91625d75ac87f5896e1
Sofia Besenski <sofia@super.gd>
2024-12-24 23:21:32 -0800

    Remove old Django templates
    We are not going to use the Django templating system for the uploader.
    I'd much rather use React completely.

--- uploader/views.py
+++ uploader/views.py
@@ -1,34 +1,20 @@
 from django.http import HttpResponse
 from uploader.models import Page, Zine 
-from uploader.serializers import ZineSerializer
-from uploader.forms import PageForm
-from django.views.generic.edit import CreateView, UpdateView
-from django.views.generic import ListView
+from uploader.serializers import ZineSerializer, PageSerializer
 
 from rest_framework import viewsets
-
-class PageCreateView(CreateView):
-    form_class = PageForm
-    template_name = "uploader/pages/new.html"
-    success_url = "/uploader/pages" 
-
-    def post(self, request, *args, **kwargs):
-        return super(PageCreateView, self).post(request, args, kwargs)
-
-
-class PageUpdateView(UpdateView):
-    form_class = PageForm 
-    template_name = "uploader/pages/edit.html"
-    success_url = "/uploader/pages" 
-
-    def get_object(self):
-        page = Page.objects.get(id=self.kwargs["id"])
-        return page 
-
-class PageIndexView(ListView):
-    model = Page 
-    template_name = "uploader/pages/index.html"  
 
 class ZineViewSet(viewsets.ModelViewSet):
     queryset = Zine.objects.all().order_by('name')
     serializer_class = ZineSerializer
+
+class PageViewSet(viewsets.ModelViewSet):
+    queryset = Page.objects.all().order_by('id')
+    serializer_class = PageSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.request.query_params.get('zine'):
+            zine = self.request.query_params.get('zine')
+
+        return queryset.filter(zine=zine) 
