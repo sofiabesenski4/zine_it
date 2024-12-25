48905c6002b9bc1b0481b2dd917f200f8840cd12
Sofia Besenski <sofia@super.gd>
2024-12-24 23:18:38 -0800

    Set up the first DRF API spec

--- integration_tests/requests/uploader_test.py
+++ integration_tests/requests/uploader_test.py
@@ -2,36 +2,20 @@ from django.test import RequestFactory, TestCase, Client
 from uploader.models import Zine, Page
 from uploader.views import PageIndexView, PageCreateView
 
+from rest_framework.test import APIRequestFactory, APIClient
+
 class UploaderRequestTest(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
+        self.api_factory = APIRequestFactory()
 
-    def test_index(self):
+    def test_api_get_zines(self):
         my_first_zine = Zine(name="My First Zine")
         my_first_zine.save()
 
-        page = Page(index=1, zine=my_first_zine).save()
-
-        # Create an instance of a GET request.
-        request = self.factory.get("/uploader/pages")
-
-        # Use this syntax for class-based views.
-        response = PageIndexView.as_view()(request)
-        self.assertEqual(response.status_code, 200)
-        
-        response.render()
+        api_client = APIClient()
+        response = api_client.get("/uploader/zines.json")
 
         self.assertContains(response, "My First Zine")
-        self.assertContains(response, "Index: 1")
-    
-    def test_create_page(self):
-        my_first_zine = Zine(name="My First Zine")
-        my_first_zine.save()
-
-        request = self.factory.post("/uploader/pages", data={"zine": my_first_zine.id, "index": "2"})
-
-        response = PageCreateView.as_view()(request)
-        response.client = Client()
         
-        self.assertRedirects(response, "/uploader/pages")
