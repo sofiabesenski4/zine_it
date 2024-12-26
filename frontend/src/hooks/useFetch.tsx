634fe16a256dfa12f54d7624b92f6b643c2b0997
Sofia Besenski <sofia@super.gd>
2024-12-26 09:07:35 -0800

    Add type to the useFetch hook

--- frontend/src/hooks/useFetch.tsx
+++ frontend/src/hooks/useFetch.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
     
-function useFetch(url) {
+function useFetch(url: String) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   
