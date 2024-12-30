bde2e2d7629c9c6e9a53f08c3c1b31988cffc8bc
Sofia Besenski <sofia@super.gd>
2024-12-29 23:35:38 -0600

    Add assertion to E2E specs
    This ensures the Anarchy 300 zine does not already exist before creating
    it in the feature spec.

--- cypress/e2e/uploader_spec.cy.js
+++ cypress/e2e/uploader_spec.cy.js
@@ -2,6 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     // TODO: Get SSL set up.
     cy.visit('http://localhost:3000/')
+    cy.contains('Anarchy 300').should('not.exist')
+
     cy.contains('New Zine').click()
     cy.get('input').type('Anarchy 300')
     cy.contains('Save').click()
@@ -11,6 +13,5 @@ describe('template spec', () => {
 
     cy.contains('Anarchy 300').click()
     cy.contains('Delete Zine').click()
-    
   })
 })
