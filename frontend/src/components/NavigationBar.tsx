863a407c84dc924e24aec96117676ddea25b34fb
Sofia Besenski <sofia@super.gd>
2024-12-31 14:30:03 -0600

    Fix SVG TSC errors

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import HeroBanner from './HeroBanner';
-import squatterZ from '../squatter-z.svg';
 
 type NavigationBarProps = {
   showHeroBanner: boolean;
@@ -9,7 +8,9 @@ type NavigationBarProps = {
 const NavigationBar: React.SFC<NavigationBarProps> = (props) => {
   return (
     <div className='flex flex-col items-center justify-start gap-8 w-full'>
-      <img src={squatterZ} className='App-logo self-start h-10 w-10' alt='logo' />
+      <div className='App-logo self-start h-10 w-10' alt='logo'>
+        <svg src='../squatter-z.svg' />
+      </div>
       {props.showHeroBanner ? (
         <HeroBanner>
           <div className='m-2'>Open Source Zine Photocopier</div>
