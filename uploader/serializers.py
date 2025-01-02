cd79a929f9410e5606be06a14c23ee358765008f
Sofia Besenski <sofia@super.gd>
2025-01-01 18:51:49 -0600

    Add Page list to serialized Zine

--- uploader/serializers.py
+++ uploader/serializers.py
@@ -3,13 +3,16 @@ from .models import Zine, Page
 from rest_framework import serializers
 
 
-class ZineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Zine 
-        fields = ['id', 'name']
 
 class PageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Page 
         fields = ['id', 'zine', 'index', 'image']
+
+class ZineSerializer(serializers.ModelSerializer):
+    pages = PageSerializer(many=True)
+
+    class Meta:
+        model = Zine 
+        fields = ['id', 'name', 'pages']
 
