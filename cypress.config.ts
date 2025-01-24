6d1e1ce73a19d12eb5d500ea16fc05d8c0672e03
Alistair Norman <alistair@super.gd>
2025-01-24 11:54:59 -0800

    Change default port to 3001

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   defaultCommandTimeout: 1000,
   e2e: {
-    baseUrl: 'http://localhost:3000',
-  },
-})
+    baseUrl: 'http://localhost:3001'
+  }
+});
