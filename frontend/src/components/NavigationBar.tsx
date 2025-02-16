570ae20c37397b3920c649283a5cc60c6898fe1b
Sofia Besenski <sofia@super.gd>
2025-02-15 21:41:40 -0800

    Move the logo in the HeroBanner

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -9,13 +9,20 @@ type NavigationBarProps = {
 const NavigationBar: React.FC<NavigationBarProps> = (props) => {
   return (
     <div className='flex flex-col items-center justify-start gap-8 w-full'>
-      <div className='App-logo self-start h-10 w-10'>
-        <img src={SquatterZ} />
-      </div>
+      <div className='App-logo self-start h-10 w-10'></div>
       {props.showHeroBanner ? (
-        <HeroBanner>
-          <div className='m-2'>Open Source Zine Photocopier</div>
-        </HeroBanner>
+        <div className='relative'>
+          <HeroBanner>
+            <img
+              src={SquatterZ}
+              width={50}
+              height={50}
+              className='App-logo absolute top-2 left-2'
+            />
+
+            <div>Open Source Zine Photocopier</div>
+          </HeroBanner>
+        </div>
       ) : null}
     </div>
   );
